9abc47901201d49b8639210791fee57129a3e453
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2026-03-04 08:04:35 -0500

    lab init

---
+++ Labs/ClassesWithResource/Label.h
@@ -0,0 +1,15 @@
+#ifndef SENECA_LABEL_H
+#define SENECA_LABEL_H
+#include <iostream>
+namespace seneca {
+    class Label {
+        char m_fill{};
+        char* m_text{};
+    public:
+        Label(const char* text = nullptr, char fill = '*');
+        std::ostream& print(std::ostream& ostr = std::cout)const;
+    };
+}
+#endif // !SENECA_LABEL_H
+
+
---
+++ Labs/ClassesWithResource/main.cpp
@@ -0,0 +1,8 @@
+#include "Label.h"
+using namespace seneca;
+int main() {
+    Label L("OOP244 Classes with resourcea;lskdjf;laskjd;flkasjd;lfkja;slkfdj;salkdjf;lksajd;lfkjsa;ldkfj;alskjf;lakjdsf;lakjdsl");
+  
+    L.print();
+    return 0;
+}
