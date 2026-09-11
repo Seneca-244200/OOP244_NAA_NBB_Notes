6232652b0c79de2d70ff8cd113d038d65e510ce9
Fardad Soleimanloo <fardad@fardad.com>
2026-09-11 14:37:47 -0400

    corrected the a zero output

--- NAA/b-sep11/a0.cpp
+++ NAA/b-sep11/a0.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main() {
    cout << "#### Assignment Zero ####" << endl;
-      cout << "   Subject and Section:" << endl;
+      cout << "Subject and Section:" << endl;
       cout << "OOP244naa" << endl;
-      cout << "Name :" << endl;
+      cout << "Name:" << endl;
       cout << "Fardad Soley" << endl;
-      cout << "Student Number :" << endl;
+      cout << "Student Number:" << endl;
       cout << "999,999,999" << endl;
-      cout << "Email :" << endl;
+      cout << "Email:" << endl;
       cout << "fsoleima@myseneca.ca" << endl;
-      cout << "Github repository URL :" << endl;
+      cout << "Github repository URL:" << endl;
       cout << "https://github.com/whatever/OOP244-Works" << endl;
    return 0;
 }
