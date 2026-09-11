59b219c8bd406943f2bc669bbd16385fb5653d50
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2026-09-11 14:29:13 -0400

    assign zero

---
+++ NAA/b-sep11/a0.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "#### Assignment Zero ####" << endl;
+      cout << "   Subject and Section:" << endl;
+      cout << "OOP244naa" << endl;
+      cout << "Name :" << endl;
+      cout << "Fardad Soley" << endl;
+      cout << "Student Number :" << endl;
+      cout << "999,999,999" << endl;
+      cout << "Email :" << endl;
+      cout << "fsoleima@myseneca.ca" << endl;
+      cout << "Github repository URL :" << endl;
+      cout << "https://github.com/whatever/OOP244-Works" << endl;
+   return 0;
+}
