b03417c5c33742d4d545572e9c05717a5605239a
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2026-09-14 12:14:44 -0400

    diff of printing in Cpp and C

---
+++ NBB/a-sep14/01-OOprint.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Hello oop" << 244 << endl;
+   return 0;
+}
---
+++ NBB/a-sep14/prg.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+using namespace std;
+int main() {
+   printf("Hello oop%d\n", 244);
+   return 0;
+}
